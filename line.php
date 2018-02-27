90a2da5049a84f3b46cbd0aa0b3bb7b510de5ca1
Phuwamet <ra.phuwamet_st@tni.ac.th>
2018-02-27 18:20:31 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = '7gIHMJYwjdr+PVaedUU8rL4V2dy2+ybg1gT6UGApOlTpSBmBxVoJjmzmdZGBgbTnPKmzbgan5zJwDqxe4kGwGruCVUzcvoG4o9N3+gXp4W1I9MXgOMpXoJQoOPeY3+9lYSIWk/MdC87T7jg/bHFVHwdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'U2cbd13188b55f3f1ce4692b15913f915',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
