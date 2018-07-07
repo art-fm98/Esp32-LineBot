b8d066d7a6ac0dba5188c89cf680ea31bedb86a1
art-fm98 <40850614+art-fm98@users.noreply.github.com>
2018-07-07 12:19:59 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -6,11 +6,11 @@ $strAccessToken = '9eKp8vy8uS5ZFWy+PAnIqYqZJjifr/k8h1iiRQiNn9zRHIOCwWdbT0CYDjZWJ
 $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
  
-$strUrl = "https://api.line.me/v2/bot/message/reply";
+$strUrl = 'https://api.line.me/v2/bot/message/reply';
  
 $arrHeader = array();
-$arrHeader[] = "Content-Type: application/json";
-$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+$arrHeader[] = 'Content-Type: application/json';
+$arrHeader[] = 'Authorization: Bearer {$strAccessToken}';
  
 if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
